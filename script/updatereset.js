ed9380f598bef4106558437c230be8b798c90f7a
Leonard Schmidt <61120933+FlamingLeo@users.noreply.github.com>
2020-03-06 10:02:15 +0200

    Delete updatereset.js

--- script/updatereset.js
+++
@@ -1,7 +0,0 @@
-function clearValues(){
-    var userConfirm = confirm("Are you sure you want to delete entries? This cannot be undone.");
-    if(userConfirm == true){
-        localStorage.removeItem("entry");
-        location.reload();
-    }
-}
