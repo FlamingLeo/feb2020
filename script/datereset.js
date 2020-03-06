6a116d0d97f5afa8ec0579ad1c70cf5964b4cf0b
Leonard Schmidt <61120933+FlamingLeo@users.noreply.github.com>
2020-03-06 10:02:06 +0200

    Delete datereset.js

--- script/datereset.js
+++
@@ -1,9 +0,0 @@
-function clearValues(){
-    var userConfirm = confirm("Are you sure you want to delete all modified values? This cannot be undone.");
-    if(userConfirm == true){
-        localStorage.removeItem("modified");
-        localStorage.removeItem("modifiedDay");
-        localStorage.removeItem("modifiedHour");
-        location.reload();
-    }
-}
